4b876f129e79e4778028945941ae9981a9f7627a
posiek07 <daniel.smolinski@hotmail.com>
2020-06-20 13:39:27 +0100

    Redux store rebuilded with hooks

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter } from 'react-router-dom';
+import ProductsProvider from './context/products-context'
+import './index.css';
+import App from './App';
+import configureStore from './hooks-store/products-store'
+
+configureStore()
+
+ReactDOM.render(
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>,
+  document.getElementById('root')
+);
